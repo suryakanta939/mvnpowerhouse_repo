dd80aeb3f36cf92f2bd69f05b97528b06f234613
suryakanta939 <suryakanta939@gmail.com>
2017-09-01 13:51:29 +0530

    commiting to the git hub

--- gitpowerhouse/src/test/java/com/powerhouse/pageClasses/UpgradeGold.java
+++ gitpowerhouse/src/test/java/com/powerhouse/pageClasses/UpgradeGold.java
@@ -1,6 +1,7 @@
 package com.powerhouse.pageClasses;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,8 +18,10 @@ public class UpgradeGold  {
 	 static WebDriver driver=null;
 	 static	ExtentTest test;
 	 PowersiteSubscription ps;
+	 AddingSite as;
 	public UpgradeGold(WebDriver driver,ExtentTest test){
 		ps=new PowersiteSubscription(driver, test);
+		 as=new AddingSite(driver,test);
 		this.driver=driver;
 		this.test=test;
 		
@@ -61,7 +64,7 @@ public class UpgradeGold  {
 	 * selecting the sites
 	 * */
 	public static WebElement selectSite(){
-		element=driver.findElement(By.id("woo_field_site']"));
+		element=driver.findElement(By.xpath("//select[@id='woo_field_site']"));
 		return element;
 	}
 	
@@ -81,10 +84,40 @@ public class UpgradeGold  {
 		return noOfSite;
 	}
 	
-	public void selectingSite(){
+	/*
+	 * this function is to add the site to the upgrade function
+	 * */
+	
+	public static void choosingSiteToAdd(){
+		Select sel=new Select(selectSite());
+		List<WebElement> options=sel.getOptions();
+		//selecting the lastsite name
+		options.get(options.size()-1).click();
+	}
+	public void selectingSite() throws InterruptedException{
 		if(noOfSitePresent()==1 && clcikTocreateSiteMessage().isDisplayed()){
 			clcikTocreateSite().click();
-			
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			test.log(LogStatus.INFO, "chekcing the no of site present");
+			as.createSiteBySubDomain();
+			test.log(LogStatus.INFO, "creating site by using domain");
+			Thread.sleep(2000);
+			driver.navigate().to("https://pohostaging.com/product/powersite-upgrade-gold/");
+			choosingSiteToAdd();
+		}else{
+			choosingSiteToAdd();
+			test.log(LogStatus.INFO, "choosed the site to the site");
 		}
+	}
+	
+	/*
+	 * this function is to upgrade the gold
+	 * */
+	
+	public void upgradegold() throws InterruptedException{
+		clickOnGoldCart();
+		test.log(LogStatus.INFO, "clicked on the gold add to cart");
+		selectingSite();
+		test.log(LogStatus.INFO, "selected the site");
 	}
 }
